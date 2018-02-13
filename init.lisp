090ec835cd3aea35071364a11b8424334580f3d7
Tomas Zellerin <zellerin@charon.lab>
2018-02-13 07:33:40 -0500

    Format change to trigger build.

--- init.lisp
+++ init.lisp
@@ -1,6 +1,9 @@
 (load "quicklisp/setup")
 (asdf:disable-output-translations)
 (ql:quickload 'hunchentoot)
-(hunchentoot:start (make-instance 'hunchentoot:easy-acceptor :port (parse-integer (sb-posix:getenv "PORT"))))
+(hunchentoot:start
+ (make-instance 'hunchentoot:easy-acceptor
+		:port (parse-integer (sb-posix:getenv "PORT"))))
 (loop
       (sleep 1000))
+
