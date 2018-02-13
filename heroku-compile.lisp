039e08671732a9a9dd0eefe38a5889e7641753b0
Tomas Zellerin <zellerin@charon.lab>
2018-02-13 06:46:40 -0500

    Test default app

---
+++ heroku-compile.lisp
@@ -0,0 +1,5 @@
+(require 'asdf)
+(asdf::disable-output-translations)
+(require-quicklisp)
+(ql:quickload 'hunchentoot)
+
---
+++ init.lisp
@@ -0,0 +1,6 @@
+(load "quicklisp/setup")
+(asdf:disable-output-translations)
+(ql:quickload 'hunchentoot)
+(hunchentoot:start (make-instance 'hunchentoot:easy-acceptor :port (parse-integer (sb-posix:getenv "PORT"))))
+(loop
+      (sleep 1000))
